8c8ff3b0b34493d6a15f022723cfb9093f778221
tgiannotta <t.giannotta92@gmail.com>
2022-01-25 15:49:22 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Google FAQ</title>
+</head>
+<body>
+
+<header>
+    <div class="header-container">Prova Header
+
+    </div>
+</header>
+<main>
+    <div class="main-container">
+        Prova MAin
+    </div>
+</main>
+
+<footer>
+    <div class="footer-container">
+        Prova Footer
+    </div>
+</footer>
+
+
+
+</body>
+</html>
