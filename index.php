47a736f96a17da76b7badbc05a1ca49f2c5806f6
tgiannotta <t.giannotta92@gmail.com>
2022-01-25 16:21:36 +0100

    end header

--- index.php
+++ index.php
@@ -5,12 +5,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Google FAQ</title>
 </head>
 <body>
 
 <header>
     <div class="header-container">
+        <div class="privacy">
+            <div class="privacy-logo">
+                <h4>Google Privacy e termini</h4>
+            </div>
+            <div class="icone">
+                <div class="menu-icona"><i class="fas fa-bars"></i></div>
+                <div class="account-icona">
+                    <img src="img/account.png" alt="img#1">
+                </div>
+            </div>
+        </div>
+        <nav>
+               <ul>
+                   <li>
+                       <a href="#">Introduzione</a>
+                   </li>
+                   <li>
+                       <a href="#">Norme sulla privacy</a>
+                   </li>
+                   <li>
+                       <a href="#">Termini di servizio</a>
+                   </li>
+                   <li>
+                       <a href="#">Tecnologie</a>
+                   </li>
+                   <li>
+                       <a class="active" href="#">Domande Frequenti</a>
+                   </li>
+               </ul>
+           </nav>
 
     </div>
 </header>
